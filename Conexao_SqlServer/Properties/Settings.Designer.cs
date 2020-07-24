c2d849bd89c844dbf58ce3406ceb14a646b0cf63
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2020-07-24 08:56:22 -0300

    C#

--- Conexao_SqlServer/Properties/Settings.Designer.cs
+++ Conexao_SqlServer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Conexao_SqlServer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace Conexao_SqlServer.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-PP6GUSQU;Initial Catalog=DBCliente;User ID=sa;Password=Paradox" +
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-M7HG815;Initial Catalog=DBCliente;User ID=sa;Password=Paradox" +
             "o22")]
         public string String {
             get {
